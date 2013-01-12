81e8b02bce792c598c52850e0c507c128f3718a4
Zack Newman <znewman01@gmail.com>
2013-01-12 12:03:23 -0500

    add VotrLib

---
+++ VotrLib/src/com/votr/Vote.java
@@ -0,0 +1,20 @@
+package com.votr;
+
+import java.util.Collection;
+
+public class Vote
+{
+	public Vote(String voter_id, String poll_id, String choice, Collection<String> tags) {
+		this.voter_id = voter_id;
+		this.poll_id = poll_id;
+		this.tags = tags;
+		this.choice = choice;
+	}
+	final String voter_id; 
+	final String poll_id; 
+	final Collection<String> tags; 
+	final String choice; 
+	
+	
+}
+
