a66c5cbe7169878c13618bd335a1089c57e15dc5
Zack Newman <znewman01@gmail.com>
2013-01-12 14:18:28 -0500

    ACTUALLY fix Vote class

--- VotrLib/src/com/votr/Vote.java
+++ VotrLib/src/com/votr/Vote.java
@@ -1,25 +1,23 @@
-package com.votr;
-
 import java.util.Collection;
 
 public class Vote
 {
-	public Vote(String voter_id, String poll_id, String choice, Collection<String> tags, String voter_city, String voter_state, String voter_zip) {
+	public Vote(String voter_id, String poll_id, String choice, String city, String state, String zipcode, Collection<String> tags) {
 		this.voter_id = voter_id;
 		this.poll_id = poll_id;
 		this.tags = tags;
 		this.choice = choice;
-		this.voter_city = voter_city;
-		this.voter_state = voter_state;
-		this.voter_zip = voter_zip;
+		this.city = city;
+		this.state = state;
+		this.zipcode = zipcode;
 	}
 	final String voter_id; 
 	final String poll_id; 
 	final Collection<String> tags; 
 	final String choice; 
-	final String voter_city;
-	final String voter_state;
-	final String voter_zip;
-	
-}
+	final String city;
+	final String state;
+	final String zipcode;
 
+
+}
